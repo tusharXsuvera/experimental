a661bec53cf91c3fd06954746a89dce9e8fcec04
tusharXsuvera <developer1.suvera@gmail.com>
2024-10-28 12:49:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import "./css/Home.css";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="para">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
