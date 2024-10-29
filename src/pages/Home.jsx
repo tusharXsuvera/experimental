510b643b06f7d85a65b03d7023f085f7118942af
tusharXsuvera <developer1.suvera@gmail.com>
2024-10-29 16:33:59 +0530

    # Concept of space between

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,19 @@ export default function Home() {
         </div>
         <div className="left_item">All item </div>
       </div>
+      <hr />
+      <div className="container2">
+        <div className="item">
+          Quality You Can Trust: We source our meats from trusted local farms
+          that prioritize animal welfare and sustainable practices. Freshness
+          Guaranteed: Our products are processed and packaged with care,
+          ensuring you receive only the freshest cuts. Variety for Every Taste:
+          From grass-fed eggs, fish, and organic chicken to specialty meats and
+          seasonal selections, our diverse range caters to every palate.
+        </div>
+        <div className="item">item 2</div>
+        <div className="item">item 3</div>
+      </div>
     </>
   );
 }
