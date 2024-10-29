362a5c52690d987ee2da55c3f30427de804ec101
tusharXsuvera <developer1.suvera@gmail.com>
2024-10-29 15:15:56 +0530

    # Concept of flex

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="para_div">
-      <p className="para">Click on the Vite and React logos to learn more</p>
-    </div>
+    <>
+      <div className="para_div">
+        <p className="para">Click on the Vite and React logos to learn more</p>
+      </div>
+      <div className="container">
+        <div className="left_item ">
+          <div className="item">item 1</div>
+          <div className="item">item 2</div>
+          <div className="item">item 3</div>
+          <div className="item">item 4</div>
+        </div>
+        <div className="left_item">All item </div>
+      </div>
+    </>
   );
 }
